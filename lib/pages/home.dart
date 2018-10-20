6c285edc166bbe31ef2f5e662247d42de477a31d
Sean <dev@nyayozangu.com>
2018-10-20 16:00:28 +0300

    linki v0.0.5 code review - reviewed homepage

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,19 +1,18 @@
-
 import 'package:flutter/material.dart';
 import 'package:linki/pages/home_page_content.dart';
 import 'package:linki/values/strings.dart';
-
-
 
 class Linki extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       title: APP_NAME,
-      theme:  ThemeData(
-          primarySwatch: Colors.lightBlue, brightness: Brightness.dark),
-      home:  MyHomePage(title: APP_NAME),
+      theme: ThemeData(
+          //todo work on the app theme data
+          primarySwatch: Colors.lightBlue,
+          brightness: Brightness.dark),
+      home: MyHomePage(title: APP_NAME),
     );
   }
 }
--- lib/pages/home_page_content.dart
+++ lib/pages/home_page_content.dart
@@ -1,16 +1,10 @@
-import 'dart:async';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
 import 'package:linki/models/link.dart';
 import 'package:linki/pages/search_dialog.dart';
 import 'package:linki/values/strings.dart';
-import 'package:linki/views/AddLinkFAB.dart';
 import 'package:linki/views/link_item_view.dart';
 import 'package:url_launcher/url_launcher.dart';
-//import 'package:html/dom.dart';lsof -wni tcp:60440lsof -wni tcp:60440
-//import 'package:html/dom.dart';
 
 const tag = 'MyHomePage:';
 
@@ -19,72 +13,20 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  var linkList = new List<Link>();
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
-        leading: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: new Image.asset(
-            APP_ICON,
-            scale: 0.1,
-          ),
-        ),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.info),
-            onPressed: () => _showInfoDialog(context),
-          ),
-          IconButton(
-              icon: new Icon(Icons.search),
-              onPressed: () => _openSearchDialog(
-                  context, linkList)) /*_openSearchDialog(context, linkList))*/
-        ],
-      ),
-      body: new StreamBuilder(
-        stream: Firestore.instance
-            .collection(LINKS_COLLECTION)
-            .orderBy(CREATED_AT, descending: true)
-            .snapshots(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData)
-            return Center(child: new CircularProgressIndicator());
+  var linkList = List<Link>();
 
-          //add links to link list for local search
-          for (DocumentSnapshot document in snapshot.data.documents) {
-            var linkId = document.documentID;
-            var link = Link.fromSnapshot(document);
-            link.id = linkId;
-            linkList.add(link);
-          }
-
-          return new ListView.builder(
-            itemCount: snapshot.data.documents.length /*linkList.length*/,
-            itemBuilder: (context, index) {
-              var document = snapshot.data.documents[index];
-              var link = Link.fromSnapshot(document);
-              return LinkItemView(link /*linkList[index]*/);
-            },
-          );
-        },
-      ),
-//        floatingActionButton: AddLinkFAB()
-    );
-  }
-
-  void _showInfoDialog(BuildContext context) async {
+  //todo test if info dialog still works
+  _showInfoDialog() async {
     await showDialog(
         context: context,
         builder: (context) {
           return AlertDialog(
-            title: new Text(appInfoText),
-            content: new Text(devByText),
+            title: Text(appInfoText),
+            content: Text(devByText),
             actions: <Widget>[
               FlatButton(
                 child: Text('Contact us'),
@@ -98,6 +40,65 @@ class _MyHomePageState extends State<MyHomePage> {
         });
   }
 
+  _openSearchDialog() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+          builder: (context) => SearchDialog(linkList), fullscreenDialog: true),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Image.asset(
+            APP_ICON,
+            scale: 0.1,
+          ),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.info),
+            onPressed: () => _showInfoDialog(),
+          ),
+          IconButton(
+              icon: Icon(Icons.search), onPressed: () => _openSearchDialog())
+        ],
+      ),
+      body: StreamBuilder(
+        stream: Firestore.instance
+            .collection(LINKS_COLLECTION)
+            .orderBy(CREATED_AT, descending: true)
+            .snapshots(),
+        builder: (_, snapshot) {
+          if (!snapshot.hasData)
+            return Center(child: CircularProgressIndicator());
+
+          //add links to link list for local search
+          for (DocumentSnapshot document in snapshot.data.documents) {
+            var linkId = document.documentID;
+            var link = Link.fromSnapshot(document);
+            link.id = linkId;
+            linkList.add(link);
+          }
+
+          return ListView.builder(
+            itemCount: snapshot.data.documents.length,
+            itemBuilder: (_, index) {
+              var document = snapshot.data.documents[index];
+              var link = Link.fromSnapshot(document);
+              return LinkItemView(link);
+            },
+          );
+        },
+      ),
+    );
+  }
+
   void _initiateContact() async {
     var url = 'tel: +2550713810803';
     if (await canLaunch(url)) {
@@ -105,14 +106,5 @@ class _MyHomePageState extends State<MyHomePage> {
     } else {
       throw 'Could not launch $url';
     }
-  }
-
-  _openSearchDialog(BuildContext context, List<Link> linkList) {
-    Navigator.push(
-      context,
-      new MaterialPageRoute(
-          builder: (context) => new SearchDialog(linkList),
-          fullscreenDialog: true),
-    );
   }
 }
